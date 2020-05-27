b1b1eb897c1e56e8eb1aa2ee5bd22b73bf9935f9
ruth6666 <62940464+ruth6666@users.noreply.github.com>
2020-05-27 12:46:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+
+function setup() {
+  createCanvas(1200,600 );
+  background(0);
+  noStroke();
+
+  fill(150);
+  rect(400, 280, 320, 300)
+  rect(300, 150, 70, 430)
+  rect(750, 150, 70, 430)
+  rect(150, 110, 100, 470)
+  rect(870, 110, 100, 470)
+  fill("blue")
+  rect(490, 300, 5, 100)
+  rect(490, 300, 100, 5)
+  rect(590, 300, 5, 100)
+  rect(490, 400, 105, 5)
+  rect(540, 300, 5, 100)
+  rect(490, 350, 105, 5)
+  rect(490, 440, 5, 140)
+  rect(490, 440, 100, 5)
+  rect(590, 440, 5, 140)
+}
+function draw(){
+drawSprites();
+}
